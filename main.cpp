c5fa1543815029c8544db27f8b29fd7b065f51c6
Juliana Barros <84809365+jubarrosousa@users.noreply.github.com>
2023-10-02 18:56:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,93 @@
-#include "classes.h"
+#include "biblioteca.h"
+#include <ctime>
+#include <stdlib.h>
 
 using namespace std;
 
-//numero de vertices, numero de arestas, grau minimo, grau maximo, grau medio, e mediana do grau
-//representar grafo com uma matriz de adjacencia e vetor de adjacencia
-//BFS, DFS, árvore geradora com níveis
-//calcular distância entre vértices (BFS) e diâmetro
-//Número de componentes conexas, tamanho (qtde de vértices) e lista de vértices da componente (por ordem de tamanho)
 
-//Quantidade de memória usada pela matriz x vetor de adj
-//verificar tempo médio- 100 BFS e DFS (sem considerar tempo de leitura e escrita)
-//pai dos vertices 10, 20, 30 pela BFS e DFS iniciando por 1,2,3
-//distância entre os vértices (10,20), (10,30), (20,30)
-//quantas componentes conexas, qual o tamanho da maior e menor componente
-//diametro por algoritmo aproximativo
- 
-int main()
-{
-    grafo grafo;
-
-    int n, v1, v2;
-
-    ifstream arquivo("teste.txt");
-    
-    arquivo >> n;
-    grafo.num_vertices = n; 
-
-    vector<vector<int>> matriz_adj(n, vector<int>(n, 0));
-    vector<int> grau_vertices(n, 0);
-    vector<vector<int>> vetor_adj(n);
-    
-    while (arquivo >> v1 >> v2) {
-        //Incrementando grau do vertice ao ler uma aresta que ele pertence
-        cout<< v1 << " ";
-        cout<< v2 <<endl;
-
-        grau_vertices[v1 -1]++;
-        grau_vertices[v2 -1]++;
-
-        grafo.num_arestas ++;
-
-        matriz_adj[v1-1][v2-1] = 1;
-        matriz_adj[v2-1][v1-1] = 1;
-
-        vetor_adj[v1-1].push_back(v2-1);
-        vetor_adj[v2-1].push_back(v1-1);
-    }
-
-    cout<< "num arestas " << grafo.num_arestas << endl;
-    
-    for (int i = 0; i < grau_vertices.size(); i++)
-        cout << grau_vertices[i] << " "; 
-
-
-    // Imprime a matriz
-    cout << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << vetor_adj[i][j] << "\t"; // \t para espaçamento entre elementos
-        }
-        cout << endl; // Nova linha após cada linha da matriz
-    }
-
-
-    return 0;
+void mostrarMenu () {
+    cout << "Escolha a operacao desejada:\n\n" <<endl
+    << "1 - Busca em largura \n" <<endl
+    << "2 - Busca em profundidade\n" <<endl
+    << "3 - Distâncias.\n" <<endl
+    << "4 - Diâmetro.\n" <<endl
+    << "5 - Componentes conexos.\n" <<endl
+    << "6 - Sair.\n" <<endl;
 }
+
+ int main() {
+
+   int n, numero_vertice, numero_vertice2, opcao, escolha;
+   string arq_texto;
+  
+   cout << "Insira o nome do arquivo texto a ser lido: ";
+   cin >> arq_texto;
+   cin.ignore(100, '\n');
+  
+   if (arq_texto.substr(arq_texto.size() - 4) != ".txt") {
+         cout << "O nome do arquivo não continha '.txt', adicionado."<<endl;
+         arq_texto =  arq_texto + ".txt";
+   }
+  
+   cout << "Voce deseja trabalhar com matriz ou vetor de adjacência? \n 1 - Matriz \n 2 - Vetor" <<endl;
+   cin >> escolha;
+  
+   grafo grafo(arq_texto, escolha);
+   n = grafo.num_vertices;
+
+   grafo.Calculo_Graus();
+
+       do {
+         mostrarMenu();
+           vector<int> pai(n, 0);
+           vector<int> nivel(n, 0);
+           vector<int> explorados(n, 0);
+
+         cin >> opcao;
+         cin.ignore(50, '\n');
+
+         switch (opcao){
+          
+         case 1:
+             cout << "Insira o numero do vertice a ser utilizado como inicial: ";
+             cin >> numero_vertice;
+            
+             grafo.BFS(numero_vertice, pai, nivel, explorados);
+          
+             break;
+          
+         case 2:
+             cout << "Insira o numero do vertice a ser utilizado como inicial: ";
+             cin >> numero_vertice;
+          
+             grafo.DFS(numero_vertice, pai, nivel, explorados);
+             break;
+         
+         case 3:
+             cout << "Insira o numero do vertice 1: ";
+             cin >> numero_vertice;
+             cout << "Insira o numero do vertice 2: ";
+             cin >> numero_vertice2;
+             cout<<grafo.Distancia(numero_vertice, numero_vertice2)<<endl;
+             break;
+          
+         case 4:
+             cout << grafo.Diametro() <<endl;
+             break;
+          
+         case 5:
+             grafo.Componente_Conexa();
+             break;
+          
+         case 6:
+             break;
+          
+         default:
+             cout << "Por favor, selecione uma dentre as opcoes validas!" <<endl;
+             break;
+         }
+
+     }while (opcao != 6);
+   return 0;
+ }
+
