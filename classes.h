11b7660c987822a012a2ad0af592817bd5daec64
Juliana Barros <84809365+jubarrosousa@users.noreply.github.com>
2023-10-02 18:51:37 -0300

    Update classes.h

--- classes.h
+++ classes.h
@@ -1,20 +1,34 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
+include <algorithm>
 #include <fstream>
+#include <iostream>
+#include <stdio.h>
 #include <string>
+#include <vector>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
-class grafo{
-     public:
-        int num_vertices = 0;
-        int num_arestas = 0;
-        int grau_min = 0;
-        int grau_max = 0;
-        int grau_mediana = 0;
+class grafo {
 
-        //vector<vector<int>>matriz_adj(int num_vertices);
-        vector<vector<int>>vetor_adj;
+public:
 
+  grafo(string, int);
+  int num_vertices=0;
+  int num_arestas=0;
+  int escolha = 0; //indica se o usuário quer trabalhar com vetor ou matriz
+
+  vector<vector<int>> matriz_adj;
+  vector<vector<int>> vetor_adj;
+  vector<int> grau_vertices;
+
+  int BFS(int, vector<int> &, vector<int> &, vector<int> &);
+  int DFS(int, vector<int> &, vector<int> &, vector<int> &);
+
+  void Constroi_Grafo(ifstream &);
+
+  void Calculo_Graus();
+  int Diametro();
+  int Distancia(int v1, int v2);
+  int Componente_Conexa();
 };
