81635077d1a790aceb2de795d8dbbedd12d852f0
Juliana Barros <84809365+jubarrosousa@users.noreply.github.com>
2023-10-24 20:46:48 -0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -1,29 +1,37 @@
-include <algorithm>
+#include <algorithm>
+#include <cfloat>
+#include <climits>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <queue>
+#include <stack>
 #include <stdio.h>
 #include <string>
 #include <vector>
-#include <queue>
-#include <stack>
 
 using namespace std;
 
 class grafo {
 
 public:
-
   grafo(string, int);
-  int num_vertices=0;
-  int num_arestas=0;
-  int escolha = 0; //indica se o usuário quer trabalhar com vetor ou matriz
+  int num_vertices = 0;
+  int num_arestas = 0;
+  int peso_negativo = 0;
+  int escolha = 0; // indica se o usuário quer trabalhar com vetor ou matriz
 
   vector<vector<int>> matriz_adj;
   vector<vector<int>> vetor_adj;
+  vector<vector<pair<int, float>>> vetor_adj_peso;
   vector<int> grau_vertices;
+  vector<float> pesos;
 
   int BFS(int, vector<int> &, vector<int> &, vector<int> &);
   int DFS(int, vector<int> &, vector<int> &, vector<int> &);
+  int Dijkstra(int, vector<int> &, vector<float> &);
+  int Dijkstra_Heap(int, vector<int> &, vector<float> &);
+  string Caminho_Min(int, vector<int> &);
 
   void Constroi_Grafo(ifstream &);
 
